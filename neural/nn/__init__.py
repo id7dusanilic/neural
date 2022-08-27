3ec6a004351ab3b1204973c184dcbd310c18d022
Dusan Ilic <idtronicsrs@gmail.com>
2022-08-27 20:16:47 +0200

    Removed redundant __all__

--- neural/nn/__init__.py
+++ neural/nn/__init__.py
@@ -2,13 +2,3 @@ from ._activation import LogSoftmax, ReLU, Sigmoid
 from ._loss import NLLLoss, CrossEntropyLoss
 from ._layers import Linear
 from ._module import Module
-
-__all__ = [
-    "LogSoftmax",
-    "ReLU",
-    "Sigmoid",
-    "NLLLoss",
-    "CrossEntropyLoss",
-    "Linear",
-    "Module",
-]
