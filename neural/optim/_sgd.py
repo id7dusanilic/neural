e0874d1d7565b79f6dd4bf11a13a5d985ee4a0fa
Dusan Ilic <idtronicsrs@gmail.com>
2022-09-15 17:22:35 +0200

    Switched to classical momentum in SGD

--- neural/optim/_sgd.py
+++ neural/optim/_sgd.py
@@ -11,56 +11,36 @@ class SGD(Optimizer):
                        step of the optimizer.
         lr (float): learning rate
         momentum (float): momentum factor
-        weightDecay (float): weight decay (L2 penalty)
-        dampening (float): dampening for momentum
-        nesterov (bool): enables Nesterov momentum
         maximize (bool): If True, maximize instead of minimize
     """
 
     def __init__(self, params: list, /, *,
             lr: float = 0.03,
             momentum: float = 0.0,
-            dampening: float = 0.0,
-            weightDecay: float = 0.0,
-            nesterov: bool = False,
             maximize: bool = False):
         super().__init__(params)
 
-        if nesterov and dampening != 0.0:
-            raise ValueError("Nesterov requires momentum and zero dampening")
-
         self.lr = lr
         self.momentum = momentum
-        self.dampening = dampening
-        self.weightDecay = weightDecay
-        self.nesterov = nesterov
         self.maximize = maximize
         self._prevParamsUpdate = [None] * len(self.params)
 
     def step(self):
-        for param, prevParamUpdate in zip(self.params, self._prevParamsUpdate):
+        for i, (param, prevParamUpdate) in enumerate(zip(self.params, self._prevParamsUpdate)):
             firstIteration = prevParamUpdate is None
             prevParamUpdate = np.zeros_like(param.grad) if firstIteration else prevParamUpdate
 
-            paramUpdate = param.grad.copy()
-
-            if self.weightDecay != 0.0:
-                paramUpdate = paramUpdate + self.weightDecay*param
-
             if self.momentum != 0.0:
                 if not firstIteration:
-                    temp = self.momentum*prevParamUpdate + (1-self.dampening)*paramUpdate
+                    paramUpdate = self.momentum*prevParamUpdate - self.lr*param.grad
                 else:
-                    temp = paramUpdate
-
-                if self.nesterov:
-                    paramUpdate = paramUpdate + self.momentum*temp
-                else:
-                    paramUpdate = temp
+                    paramUpdate = -self.lr*param.grad
+            else:
+                paramUpdate = -self.lr*param.grad
 
             if self.maximize:
-                param[:] = param[:] + self.lr*paramUpdate
+                param[:] = param[:] - paramUpdate
             else:
-                param[:] = param[:] - self.lr*paramUpdate
+                param[:] = param[:] + paramUpdate
 
-            prevParamUpdate = paramUpdate
+            self._prevParamsUpdate[i] = paramUpdate
